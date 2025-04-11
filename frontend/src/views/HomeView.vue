e6db37a7b95ed52faa3ab11e463c52bf18963dae
razan rumah <razan@rumah.windows>
2025-04-11 23:50:45 +0700

    mengedit frontend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+     <h2> Laravel + Vue</h2>
   </main>
 </template>
