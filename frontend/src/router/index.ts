b756e0e0234602629b2b01e9b1327b86b06062f4
razan rumah <razan@rumah.windows>
2025-04-12 02:13:11 +0700

    menambahkan UI

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Masuk from '../views/auth/Masuk.vue'
+import Daftar from '../views/auth/Daftar.vue'
+import Panel from '../views/auth/Panel.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +13,28 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/masuk',
+      name: 'masuk',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: Masuk,
+    },
+    {
+      path: '/daftar',
+      name: 'daftar',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component:Daftar,
+    },
+    {
+      path: '/panel',
+      name: 'panel',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component:Panel,
     },
   ],
 })
---
+++ frontend/src/views/auth/Panel.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+    <h1>Panel pengguna</h1>
+</template>
